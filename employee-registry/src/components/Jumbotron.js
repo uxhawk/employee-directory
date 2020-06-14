8ca02fcf0b6a348abac559122dc08d008c533f17
uxhawk <uxhawk@gmail.com>
2020-06-14 15:31:45 -0400

    created search and jumbotron components

---
+++ employee-registry/src/components/Jumbotron.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Jumbotron = () => {
+    return (
+        <div className="jumbotron jumbotron-fluid">
+            <div className="container">
+            <h1 className="display-4">Employee Directory</h1>
+            </div>
+        </div>
+    );
+};
+
+export default Jumbotron;
---
+++ employee-registry/src/components/Search.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Search = () => {
+    return (
+        <div className="row">
+            <div className="col-md-2 offset-md-5"> 
+                <div className="input-group mb-3">
+                    <input type="text" className="form-control" placeholder="Search Employees" aria-label="Recipient's username" aria-describedby="button-addon2" />
+                <div className="input-group-append">
+                    <button className="btn btn-outline-primary" type="button" id="button-addon2">Search</button>
+                </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Search;
